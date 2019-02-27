b5f3b9442148308f6ea3a1acb732c38ee6c30fce
agroniseni88 <46695046+agroniseni88@users.noreply.github.com>
2019-02-27 14:11:22 +0100

    Some changes

--- indexHM.js
+++ indexHM.js
@@ -1,12 +1,14 @@
 function doSomething()
-{  
-    //  var toDoInput=prompt('GIve a task name: ');
+{
 
-    var item=document.getElementById("toDoInput").value
-    var text=document.createTextNode(item)
-    var newItem=document.createElement("li")
-    newItem.appendChild(text)
-    document.getElementById("doSomething").appendChild(newItem)
- 
-}
+   var item=document.getElementById("toDoInput").value
+   var text=document.createTextNode(item)
+   var newItem=document.createElement("li")
+   newItem.appendChild(text)
+   document.getElementById("doSomething").prepend(newItem)
+   let count=document.getElementById("doSomething").childElementCount
+   console.log(count)
+   document.getElementById("cnt").innerText=count
+
+};
 
